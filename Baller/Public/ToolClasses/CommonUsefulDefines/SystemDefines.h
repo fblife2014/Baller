51291e1f4ce59602d71e02e6bbd5dc2e3616a439
chaoweili <lcwei0521@163.com>
2015-03-21 12:36:36 +0800

    lcw

--- Baller/Public/ToolClasses/CommonUsefulDefines/SystemDefines.h
+++ Baller/Public/ToolClasses/CommonUsefulDefines/SystemDefines.h
@@ -27,4 +27,17 @@
 
 #define Baller_RCTestToken @"oFum0Dxv6MkkcYOPskMnXFqSqg2vN7fksoctuRRZGFdeGV9sajE+ZilAAjzB7oess4eI+YwyhFRy9UdKlLevVQ==" //容云测试token
 
+//第三方appkey和appSecret
+
+#define SinaAppKey @"2208620241"
+#define SinaAppSecret @"fe596bc4ac8c92316ad5f255fbc49432"
+
+#define QQAPPID @"1104065435" //十六进制:41CEB39B; 生成方法:echo 'ibase=10;obase=16;1104065435'|bc
+#define QQAPPKEY @"UgVWGacRoeo9NtZy"
+
+#define WXAPPID @"wx47f54e431de32846"
+#define WXAPPSECRET @"a71699732e3bef01aefdaf324e2f522c"
+
+#define RedirectUrl @"http://sns.whalecloud.com/sina2/callback" //回调地址
+
 #endif
